af7427cddbd29cad1c1dfc9d78059254f7bbe0a7
Sachin Kumar <kumarsachinm@gmail.com>
2024-06-07 09:15:51 +0530

    Hero opacity

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import  hero from "../../assets/hero.png"
+import  hero from "../../assets/hero.png";
 import styles from "./Hero.module.css";
 
 export default function Hero(){
